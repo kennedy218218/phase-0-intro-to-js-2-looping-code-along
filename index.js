793c95a52772f62e437fcc87585e6eb073abb19a
kennedy <kennedy.mwai@student.moringaschool.com>
2025-03-14 08:43:24 +0300

    completed quiz

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+ function writeCards(names, event){
+
+    let messages = [];
+
+    for (let k = 0; k < names.length; k++) {
+    messages.push(`Thank you, ${names[k]}, for the wonderful ${event} gift!`);
+ }
+ return messages;
+}
+
+
+
+function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+
+
+
+
+
